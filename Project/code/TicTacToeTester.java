3d870bd2a86e15d5f889856ee315699ca86de30d
tirth99 <54487784+tirth99@users.noreply.github.com>
2020-05-11 21:09:17 -0700

    Delete TicTacToeTester.java

--- Project/code/TicTacToeTester.java
+++
@@ -1,9 +0,0 @@
-package tictactoe;
-
-public class TicTacToeTester {
-	public static void main(String[] args) {
-		Model m = new Model();
-		View v = new View();
-		Controller c = new Controller(m, v);
-	}
-}
